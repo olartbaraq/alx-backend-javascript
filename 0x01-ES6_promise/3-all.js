62481c597b6d57da6e34250e9da4f7ba2aa90995
olart <akanbi.ges201@gmail.com>
2022-06-15 22:23:23 +0100

    update task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,9 +3,10 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
-  Promise.all([promise1, promise2]).then((values) => {
-    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-  }).catch(() => {
-    console.log('Signup system offline');
-  });
+  return Promise.all([promise1, promise2])
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+    }).catch(() => {
+      console.log('Signup system offline');
+    });
 }
