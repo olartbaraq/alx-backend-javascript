e771f102e70f7bb4d7642d04f4af07990ebba479
olart <akanbi.ges201@gmail.com>
2022-06-15 21:57:34 +0100

    confrom with eslint, updated task 2 and uploaded code for task 3

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return ({ status: 200, body: 'Success' });
-  },
-  () => Error(),
-  );
+  const promiseResponse = { status: 200, body: 'success' };
+  return promise
+    .then(() => promiseResponse)
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
