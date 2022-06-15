68e89f6ff8a899aa0375cae36bbdfa5293b922a1
olart <akanbi.ges201@gmail.com>
2022-06-15 21:11:50 +0100

    added code for task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,18 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise = new Promise((resolve, reject) => {
-    const success = true;
-    if (success) {
-      resolve({
-        status: 200,
-        body: success,
-      });
-    } else {
-      reject(Error());
-    }
-    promise.then(() => {
-      const result = 'Got a response from the API';
-      console.log(result);
-    });
-    return promise;
-  });
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return ({ status: 200, body: 'Success' });
+  },
+  () => Error(),
+  );
 }
