2691c5b8fca5a4e1bd86dcf58b4746dbf49677ef
olart <akanbi.ges201@gmail.com>
2022-06-15 23:23:28 +0100

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,23 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      const array = [];
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          array.push({
+            status: result.status,
+            value: result.value,
+          });
+        } else {
+          array.push({
+            status: result.status,
+            value: `Error: ${result.reason.message}`,
+          });
+        }
+      });
+      return array;
+    });
+}
