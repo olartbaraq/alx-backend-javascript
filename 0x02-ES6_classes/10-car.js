be14ec1d1f118dfa42c98915e711571b00d447b2
olart <akanbi.ges201@gmail.com>
2022-06-18 07:10:51 +0100

    task 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,18 @@
+/* eslint no-underscore-dangle: ["error", {"allow": ["_brand", "_color", "_motor"] }] */
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species(this._brand, this._motor, this._color);
+  }
+}
