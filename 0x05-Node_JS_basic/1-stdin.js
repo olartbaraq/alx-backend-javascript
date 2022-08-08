a22542c848e80da152f4d5f725b6a6135f8e7503
olart <akanbi.ges201@gmail.com>
2022-08-08 21:10:14 +0100

    Executing basic javascript with Node JS + Using Process stdin + README

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let userInput = '';
+
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  userInput = name;
+  console.log(`Your name is: ${userInput}`);
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing\n');
+});
