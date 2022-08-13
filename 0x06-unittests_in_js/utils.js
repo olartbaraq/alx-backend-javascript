abb1ae61c02012e6265926c8510fe986c79b912c
olart <akanbi.ges201@gmail.com>
2022-08-13 22:05:20 +0100

    spies

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,23 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    const aR = Math.round(a);
+    const bR = Math.round(b);
+    const sum = 'SUM';
+    const subtract = 'SUBTRACT';
+    const divide = 'DIVIDE';
+    if (type === sum){
+      return aR + bR;
+    } else if(type === subtract) {
+      return aR - bR;
+    } else if(type === divide) {
+      if(bR === 0) {
+        return 'Error'
+      } else {
+        return aR / bR;
+      }
+    }
+        
+  },
+};
+
+module.exports = Utils;
